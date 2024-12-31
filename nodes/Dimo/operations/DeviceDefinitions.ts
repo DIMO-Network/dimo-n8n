efd91b5e19f5b04e2bcb81d3f6e015b0e034273e
Barrett <barrettkowalsky@gmail.com>
2024-12-31 11:05:10 -0700

    add search to device defs and json parse

--- nodes/Dimo/operations/DeviceDefinitions.ts
+++ nodes/Dimo/operations/DeviceDefinitions.ts
@@ -12,7 +12,6 @@ export const devicedefinitions = {
 						resource: ['devicedefinitions'],
 					},
 				},
-				// TODO: Confirm format: either developer_jwt or jwt
 				default: '={{ $json.developer_jwt }}',
 				description: 'The Developer JWT. If you do not already have one, connect a DIMO Node before this to get one.',
 				required: true,
@@ -24,7 +23,7 @@ export const devicedefinitions = {
 				displayOptions: {
 					show: {
 						resource: ['devicedefinitions'],
-						// option: ['decodeVin'],
+						operation: ['decodeVin'],
 					},
 				},
 				default: '',
@@ -38,111 +37,107 @@ export const devicedefinitions = {
 				displayOptions: {
 					show: {
 						resource: ['devicedefinitions'],
-						// option: ['decodeVin'],
+						operation: ['decodeVin'],
 					},
 				},
 				default: '',
 				description: 'The Vehicle Identifier Number',
 				required: true,
 			},
-			// {
-			// 	displayName: 'Query',
-			// 	name: 'query',
-			// 	type: 'string',
-			// 	displayOptions: {
-			// 		show: {
-			// 			resource: ['devicedefinitions'],
-			// 			option: ['search'],
-			// 		},
-			// 	},
-			// 	default: '',
-			// 	description: 'Your search query filter, e.g. Lexus gx 2023',
-			// },
-			// {
-			// 	displayName: 'Vehicle Make',
-			// 	name: 'makeSlug',
-			// 	type: 'string',
-			// 	displayOptions: {
-			// 		show: {
-			// 			resource: ['devicedefinitions'],
-			// 			option: ['search'],
-			// 		},
-			// 	},
-			// 	default: '',
-			// 	description: 'The make of the vehicle you are searching, e.g. Audi, Lexus, etc'
-			// },
-			// {
-			// 	displayName: 'Vehicle Model',
-			// 	name: 'modelSlug',
-			// 	type: 'string',
-			// 	displayOptions: {
-			// 		show: {
-			// 			resource: ['devicedefinitions'],
-			// 			option: ['search'],
-			// 		},
-			// 	},
-			// 	default: '',
-			// 	description: 'The model of the vehicle you are searching, e.g. Tacoma, Accord, etc'
-			// },
-			// {
-			// 	displayName: 'Vehicle Year',
-			// 	name: 'year',
-			// 	type: 'number',
-			// 	displayOptions: {
-			// 		show: {
-			// 			resource: ['devicedefinitions'],
-			// 			option: ['search'],
-			// 		},
-			// 	},
-			// 	default: '',
-			// 	description: 'The year of the vehicle you are searching, e.g. 2024'
-			// },
-			// {
-			// 	displayName: 'Page',
-			// 	name: 'page',
-			// 	type: 'number',
-			// 	displayOptions: {
-			// 		show: {
-			// 			resource: ['devicedefinitions'],
-			// 			option: ['search'],
-			// 		},
-			// 	},
-			// 	default: '',
-			// 	description: 'Page number (for pagniation, defaults to the first page)'
-			// },
-			// {
-			// 	displayName: 'Page Size',
-			// 	name: 'pageSize',
-			// 	type: 'number',
-			// 	displayOptions: {
-			// 		show: {
-			// 			resource: ['devicedefinitions'],
-			// 			option: ['search'],
-			// 		},
-			// 	},
-			// 	default: '',
-			// 	description: 'Page size, (to specify the number of items to show on one page)'
-			// },
+			{
+				displayName: 'Query',
+				name: 'query',
+				type: 'string',
+				displayOptions: {
+					show: {
+						resource: ['devicedefinitions'],
+						operation: ['search'],
+					},
+				},
+				default: '',
+				description: 'Your search query filter, e.g. Lexus gx 2023',
+			},
+			{
+				displayName: 'Vehicle Make',
+				name: 'makeSlug',
+				type: 'string',
+				displayOptions: {
+					show: {
+						resource: ['devicedefinitions'],
+						operation: ['search'],
+					},
+				},
+				default: '',
+				description: 'The make of the vehicle you are searching, e.g. Audi, Lexus, etc'
+			},
+			{
+				displayName: 'Vehicle Model',
+				name: 'modelSlug',
+				type: 'string',
+				displayOptions: {
+					show: {
+						resource: ['devicedefinitions'],
+						operation: ['search'],
+					},
+				},
+				default: '',
+				description: 'The model of the vehicle you are searching, e.g. Tacoma, Accord, etc'
+			},
+			{
+				displayName: 'Vehicle Year',
+				name: 'year',
+				type: 'number',
+				displayOptions: {
+					show: {
+						resource: ['devicedefinitions'],
+						operation: ['search'],
+					},
+				},
+				default: '',
+				description: 'The year of the vehicle you are searching, e.g. 2024'
+			},
+			{
+				displayName: 'Page',
+				name: 'page',
+				type: 'number',
+				displayOptions: {
+					show: {
+						resource: ['devicedefinitions'],
+						operation: ['search'],
+					},
+				},
+				default: '',
+				description: 'Page number (for pagniation, defaults to the first page)'
+			},
+			{
+				displayName: 'Page Size',
+				name: 'pageSize',
+				type: 'number',
+				displayOptions: {
+					show: {
+						resource: ['devicedefinitions'],
+						operation: ['search'],
+					},
+				},
+				default: '',
+				description: 'Page size, (to specify the number of items to show on one page)'
+			},
 		];
 	},
 
 	async execute(helper: any, operation: string) {
 		const developerJwt = helper.executeFunctions.getNodeParameter('developerJwt', 0) as string;
-		const countryCode = helper.executeFunctions.getNodeParameter('countryCode', 0) as string;
-		const vin = helper.executeFunctions.getNodeParameter('vin', 0) as string;
-		// const query = helper.executeFunctions.getNodeParameter('query', 0) as string;
-		// const makeSlug = helper.executeFunctions.getNodeParameter('makeSlug', 0) as string;
-		// const modelSlug = helper.executeFunctions.getNodeParameter('modelSlug', 0) as string;
-		// const year = helper.executeFunctions.getNodeParameter('year', 0) as number;
-		// const page = helper.executeFunctions.getNodeParameter('page', 0) as number;
-		// const pageSize = helper.executeFunctions.getNodeParameter('pageSize', 0) as number;
 
 		const basePath = helper.credentials.environment === 'Dev'
 			? 'https://device-definitions-api.dev.dimo.zone'
 			: 'https://device-definitions-api.dimo.zone';
 
+
 		switch (operation) {
 			case 'decodeVin':
+			const countryCode = helper.executeFunctions.getNodeParameter('countryCode', 0) as string;
+			const vin = helper.executeFunctions.getNodeParameter('vin', 0) as string;
+
 			const decodeVinResponse = await helper.executeFunctions.helpers.request({
 				method: 'POST',
 				url: `${basePath}/device-definitions/decode-vin`,
@@ -150,16 +145,35 @@ export const devicedefinitions = {
 					'Authorization': `Bearer ${developerJwt}`,
 					'Content-Type': 'application/json',
 				},
-				// CHECK: NEEDED??
 				body: JSON.stringify({
 					countryCode,
 					vin,
 				})
 			});
+			return JSON.parse(decodeVinResponse)
 
-			return{
-				decodeVinResponse
-			}
+			case 'search': {
+				const searchParams: Record<string, string | number> = {};
+
+				const paramList = ['query', 'makeSlug', 'modelSlug', 'year', 'page', 'pageSize'];
+				for (const param of paramList) {
+						const value = helper.executeFunctions.getNodeParameter(param, 0);
+						if (value !== null && value !== '') {
+								searchParams[param] = value;
+						}
+				}
+
+				const searchResponse = await helper.executeFunctions.helpers.request({
+						method: 'GET',
+						url: `${basePath}/device-definitions/search`,
+						headers: {
+							'Authorization': `Bearer ${developerJwt}`,
+							'Content-Type': 'application/json',
+						},
+						params: searchParams,
+				});
+				return JSON.parse(searchResponse);
+		}
 
 			default:
 				throw new Error(`The operation failed: ${operation}`)
